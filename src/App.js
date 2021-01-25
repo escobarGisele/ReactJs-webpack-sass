4e048acaf3f506a427c3fc3f5dd920dcc288c180
Gisele Escobar <escobargisele1@gmail.com>
2021-01-25 02:41:30 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+const App = () => {
+    return ( 
+        <Fragment>
+            <h1>Hello Word jkhjkhjkjhk</h1>
+        </Fragment>
+     );
+}
+ 
+export default App;
